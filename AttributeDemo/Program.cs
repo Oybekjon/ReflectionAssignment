2cc491f1a73ad19bfb233c6687c37b09145a64b1
Oybek Jumaniyozov <jumaniyozov@oybek.com>
2019-12-30 18:13:56 +0300

    Running stuff automatically

--- AttributeDemo/Program.cs
+++ AttributeDemo/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AttributeDemo.TestHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace AttributeDemo
 {
@@ -6,7 +10,42 @@ namespace AttributeDemo
     {
         static void Main(string[] args)
         {
-            
+            var result = Assembly.GetExecutingAssembly().GetTypes();
+            var allClasses = result.Where(x => !x.IsAbstract && x.IsClass).ToList();
+            var relevant = new List<Type>();
+            foreach (var item in allClasses)
+            {
+                var testAttr = item.GetCustomAttribute<TestClassAttribute>();
+                if (testAttr != null)
+                {
+                    // Class has this attribute. We need it
+                    relevant.Add(item);
+                }
+            }
+
+            foreach (var classDefinition in relevant)
+            {
+                var allMethods = classDefinition.GetMethods(BindingFlags.Public | BindingFlags.Instance).ToList();
+                var relevantMethod = new List<MethodInfo>();
+                foreach (var method in allMethods)
+                {
+                    var attr = method.GetCustomAttribute<TestMethodAttribute>();
+                    if (attr != null)
+                    {
+                        // our guy
+                        relevantMethod.Add(method);
+                    }
+                }
+
+                var instance = Activator.CreateInstance(classDefinition);
+
+                foreach (var relMeth in relevantMethod)
+                {
+                    relMeth.Invoke(instance, new object[] { });
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
